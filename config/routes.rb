85054fa2792a3bdf819664299431de113bb1c812
Adam <adam.j.mccor@gmail.com>
2022-01-18 15:12:28 +0000

    chat-12, added User Story 5 feature test, created routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   post '/users', to: 'users#create'
 
+  get '/sessions/new', to: 'sessions#new'
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -5,12 +5,26 @@ feature 'New User' do
   scenario 'User can sign up' do
     visit('/users')
 
-    fill_in('First_name', with: 'Greg')
-    fill_in('Last_name', with: 'Egg')
-    fill_in('Email', with: 'gregegg@email.com')
-    fill_in('Password', with: '12345')
-    click_button('Submit')
+    fill_in('first_name', with: 'Greg')
+    fill_in('last_name', with: 'Egg')
+    fill_in('email', with: 'gregegg@email.com')
+    fill_in('password', with: '12345')
+    click_button('Sign Up')
 
-    expect(page).to have_content 'Welcome, gregegg@email.com'
+    expect(User.last.first_name).to eq('Greg')
+  end
+end
+
+
+feature 'authentication' do
+  scenario 'User can sign in' do
+    User.create(email: 'test@email.com', password: 'password123')
+
+    visit ('/sessions/new')
+    fill_in(:email, with: 'test@email.com')
+    fill_in(:password, with: 'password123')
+    click_button('Sign in')
+
+    expect(page).to have_content('Welcome, test@email.com')
   end
 end
