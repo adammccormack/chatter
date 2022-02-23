e458e17d679e4100292a9b9c12fba4e7d50f867e
Adam <adam.j.mccor@gmail.com>
2022-02-23 11:47:18 +0000

    chat-14, user story 7 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,11 +14,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = 'User created.'
+    if @user.valid?
+      @user.save
+      flash[:notice] = 'Welcome ' + @user.username
       session[:user_id] = @user.id
       redirect_to root_path
-    else
+    elsif @user.invalid?
+      flash[:notice] = 'Username already in use, please try another one.'
       render 'new'
     end
   end
@@ -28,6 +30,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.permit(:first_name, :last_name, :email, :password)
+    params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,12 +6,14 @@ feature 'New User' do
   scenario 'User can sign up' do
     visit('/signup')
 
+    fill_in('username', with: 'Gman')
     fill_in('first_name', with: 'Greg')
     fill_in('last_name', with: 'Egg')
     fill_in('email', with: 'gregegg@email.com')
     fill_in('password', with: '12345')
     click_button('Sign Up')
 
+    expect(User.last.username).to eq('Gman')
     expect(User.last.first_name).to eq('Greg')
     expect(page).to have_content('Welcome, gregegg@email.com')
   end
